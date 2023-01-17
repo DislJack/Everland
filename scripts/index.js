ab1f39c51f9f616b623cfc557236932ef3001569
Yaroslav Shadrin <yaroslavbrok@gmail.com>
2023-01-17 14:21:59 +0300

    Промежуточный коммит. Анимация работате, но очень странно. Только 1 раз.

--- scripts/index.js
+++ scripts/index.js
@@ -10,20 +10,17 @@ const accordeons = document.querySelectorAll('.accordeon');
 
 accordeons.forEach((accordeon) => {
   const button = accordeon.querySelector('.button_type_accordeon');
-  const control = accordeon.querySelector('.accordeon__label');
   const content = accordeon.querySelector('.accordeon__text');
-  button.addEventListener('click', () => {
-    if (accordeon.hasAttribute('open') === false) {
-      accordeon.setAttribute('open', true);
-      content.style.maxHeight = content.scrollHeight + 'px';
-    } else {
-      accordeon.removeAttribute('open');
-      content.style.maxHeight = null;
-    }
-  });
-  if (accordeon.hasAttribute('open') === true) {
+  if (accordeon.hasAttribute('open')) {
     content.style.maxHeight = null;
   } else {
     content.style.maxHeight = content.scrollHeight + 'px';
   }
+  button.addEventListener('click', () => {
+    if (accordeon.hasAttribute('open') === false) {
+      accordeon.setAttribute('open', true);
+    } else {
+      accordeon.removeAttribute('open');
+    }
+  });
 });
