35af28fed6ee478beeac971c041e8ffbd4c93818
Sergey Kouzma <sergey.kouzma@gmail.com>
2023-01-17 15:28:26 +0300

    Сокращение кода

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,8 @@ const accordeons = document.querySelectorAll('.accordeon');
 accordeons.forEach((accordeon) => {
   const button = accordeon.querySelector('.button_type_accordeon');
   const content = accordeon.querySelector('.accordeon__text');
-  if (accordeon.hasAttribute('open') === false) {
-    content.style.maxHeight = content.scrollHeight + 'px';
-  } else {
-    content.style.maxHeight = null;
-  }
   button.addEventListener('click', () => {
-    if (accordeon.hasAttribute('open') === false) {
-      accordeon.setAttribute('open', true);
-    } else {
-      accordeon.removeAttribute('open');
-    }
+    accordeon.toggleAttribute('open');
+    content.toggleAttribute('open');
   });
 });
