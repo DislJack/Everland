45c2b65611502d657cb21d075ee2d2c34433d396
Egor Ezhkov <egorkashpak21@yandex.ru>
2023-01-20 04:37:36 +0300

    fix margin, fix js

--- scripts/support.js
+++ scripts/support.js
@@ -1,15 +1,18 @@
-const supportButton = document.querySelectorAll('.support__button')
-const supportButtonSum = document.querySelectorAll('.support__button-sum')
-const supportButtonPayment = document.querySelectorAll('.support__button-payment')
+const supportButtons = document.querySelectorAll('.support__button');
+const supportButtonsSum = document.querySelectorAll('.support__button-sum');
+const supportButtonsPayment = document.querySelectorAll('.support__button-payment');
+
 
 function buttonActive(button) {
 	button.forEach(el => {
-		el.addEventListener('click', function() {
-			el.classList.toggle('support__button_active')
+		el.addEventListener('click', (e) => {
+			button.forEach(item => { item.classList.remove('support__button_active'); });
+			el.classList.add('support__button_active')
 		})
 	});
-}
+};
 
-buttonActive(supportButton)
-buttonActive(supportButtonSum)
-buttonActive(supportButtonPayment)
+
+buttonActive(supportButtons);
+buttonActive(supportButtonsSum);
+buttonActive(supportButtonsPayment);
