61e40f261fd7d5bea9606e3e5872cfd671e59cbd
Mark22sv <116151585+Mark22sv@users.noreply.github.com>
2023-01-20 08:38:45 +0600

    Everland and Projects slider pull-request

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
 // Переменные Everland
 const everlandSliderImageTemplate = document.querySelector('#everland-slider-image').content;
+const projectsSliderImageTemplate = document.querySelector('#projects-slider-image').content;
 const everland = document.querySelector('.everland');
-const everlandSlider = everland.querySelector('.slider_theme-everland');
+const everlandSlider = everland.querySelector('.slider');
 const prevEverlandSlider = everland.querySelector('#prev-slide');
 const nextEverlandSlider = everland.querySelector('#next-slide');
 const sliderCounterEverland = everland.querySelector('.slider__counter');
 const sliderTitleEverland = everland.querySelector('.slider__title');
+const projects = document.querySelector('.projects');
+const projectsSlider = projects.querySelector('.slider');
+const prevProjectsSlider = projects.querySelector('#prev-slide');
+const nextProjectsSlider = projects.querySelector('#next-slide');
+const sliderTitleProjects = projects.querySelector('.slider__title');
 let sliderImageEverland;
+let sliderImageProjects;
 let counterEverland = 1;
+let counterProjects = 1;
 let numberImageEverland = 1;
 
 // Функция создания изображения Everland
@@ -17,6 +25,14 @@ function createEverlandSliderImage (firstImage, secondImage) {
   sliderImageContainer.querySelector('#second-image').style.backgroundImage = imageEverlandSlider[secondImage];
   sliderImageEverland = sliderImageContainer;
 }
+
+// Функция создания изображения Projects
+function createProjectsSliderImage (Image) {
+  const sliderImageContainer = projectsSliderImageTemplate.querySelector('.slider__image-container').cloneNode(true);
+  sliderImageContainer.querySelector('#image').style.backgroundImage = imageProjectsSlider[Image];
+  sliderImageProjects = sliderImageContainer;
+}
+
 
 // Функция добавления изображения
   function addSliderImage(classList, image) {
@@ -34,15 +50,20 @@ function addSliderCounterEverland(counter){
 }
 
 // Функция добавления описания слайда
-function addSliderTitle(classList, title){
-  classList.textContent = imageDescriptionEverland[title];
+function addSliderTitle(classList, number, title){
+  classList.textContent = title[number];
 };
 
 // Вызов функций слайдера Everland
 createEverlandSliderImage(numberImageEverland - 1, numberImageEverland);
 addSliderImage(everlandSlider, sliderImageEverland);
 addSliderCounterEverland(counterEverland);
-addSliderTitle(sliderTitleEverland, counterEverland - 1);
+addSliderTitle(sliderTitleEverland, counterEverland - 1, imageDescriptionEverland);
+
+// Вызов функций слайдера Projects
+createProjectsSliderImage(counterProjects - 1);
+addSliderImage(projectsSlider, sliderImageProjects);
+addSliderTitle(sliderTitleProjects, counterProjects - 1, imageDescriptionProjects);
 
 // Установка слушателей на кнопки слайдера Everland
 prevEverlandSlider.addEventListener('click', () => {
@@ -52,12 +73,11 @@ prevEverlandSlider.addEventListener('click', () => {
     counterEverland = 6;
     numberImageEverland = 11;
   }
-  console.log(counterEverland);
   removeSliderImage(sliderImageEverland);
   createEverlandSliderImage(numberImageEverland - 1, numberImageEverland);
   addSliderImage(everlandSlider, sliderImageEverland);
   addSliderCounterEverland(counterEverland);
-  addSliderTitle(sliderTitleEverland, counterEverland - 1);
+  addSliderTitle(sliderTitleEverland, counterEverland - 1, imageDescriptionEverland);
 });
 
 nextEverlandSlider.addEventListener('click', () => {
@@ -67,11 +87,34 @@ nextEverlandSlider.addEventListener('click', () => {
     counterEverland = 1;
     numberImageEverland = 1;
   }
-  console.log(counterEverland);
   removeSliderImage(sliderImageEverland);
   createEverlandSliderImage(numberImageEverland - 1, numberImageEverland);
   addSliderImage(everlandSlider, sliderImageEverland);
   addSliderCounterEverland(counterEverland);
-  addSliderTitle(sliderTitleEverland, counterEverland - 1);
+  addSliderTitle(sliderTitleEverland, counterEverland - 1, imageDescriptionEverland);
 });
+
+// Установка слушателей на кнопки слайдера Everland
+prevProjectsSlider.addEventListener('click', () => {
+  counterProjects -= 1;
+  if (counterProjects < 1) {
+    counterProjects = 6;
+  }
+  removeSliderImage(sliderImageProjects);
+  createProjectsSliderImage(counterProjects - 1);
+  addSliderImage(projectsSlider, sliderImageProjects);
+  addSliderTitle(sliderTitleProjects, counterProjects - 1, imageDescriptionProjects);
+});
+
+nextProjectsSlider.addEventListener('click', () => {
+  counterProjects += 1;
+  if (counterProjects > 6) {
+    counterProjects = 1;
+  }
+  removeSliderImage(sliderImageProjects);
+  createProjectsSliderImage(counterProjects - 1);
+  addSliderImage(projectsSlider, sliderImageProjects);
+  addSliderTitle(sliderTitleProjects, counterProjects - 1, imageDescriptionProjects);
+});
+
 
